12340e839820531d3c399e4a8a02ee67348118f0
Víctor Felices <vhevan15@gmail.com>
2020-03-03 15:10:15 +0100

    cambios

--- exerciciarrays4b.php
+++ exerciciarrays4b.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercici Arrays</title>
+    <title>Exercici Arrays 4b</title>
 </head>
 <body>
-
-
+    
 <?php
 
 
@@ -24,7 +23,15 @@
 
 
 
+
+
+
+
 ?>
+
+
+
+
 
 
 
